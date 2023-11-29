7a1dabe1ec522b977010a40376866d9137bf44e5
Andria Salah <74527457+AndriaSalah@users.noreply.github.com>
2023-11-29 05:16:43 +0200

    some css fixes

--- src/Components/Data/Hero/Hero.jsx
+++ src/Components/Data/Hero/Hero.jsx
@@ -1,19 +1,13 @@
 import'./Hero.scss'
-import {useContext, useState} from "react";
+import {useContext} from "react";
 import {WeatherDataContext} from "../../Main/Main.jsx";
 const Hero = () => {
 const {name} = useContext(WeatherDataContext)
-    let Iname
-    if(name){
-        if(name === "Dayana" || name === "dayana"){
-            Iname = "I love you Dayana"
-        }
-        else Iname = name
-    }
+
     return (
         <div className={"Hero"}>
             <div className={"TextContainer"}>
-                <h2>Welcome back {Iname}</h2>
+                <h2>Welcome back {name}</h2>
                 <h4>{`Check out Today's weather information`}</h4>
             </div>
             {/*<div className={"Profile_Settings"}>*/}
